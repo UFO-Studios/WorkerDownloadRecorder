7d1e7b1102da8c0cf51f26f5fc587a31377cfaef
NiceygyLive <niceygy@niceygylive.xyz>
2023-09-26 16:08:10 +0000

    Added direct download & weekly download counts

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 ![Alt text](image.png)
  
  ### Usage:
- - Change your download links to `https://Your.Worker.workers.dev/reder?url=`GH_Username/Repo<br>
- E.G: download.niceygy.worker.dev/reder?url=niceygylive/niceygylive.xyz
+ - Change your download links to `https://Your.Worker.workers.dev/reder?url=`GH_Username/Repo?file=RELEASE-NUM/FILENAME.zip<br>
+ E.G: download.niceygy.worker.dev/reder?url=niceygylive/niceygylive.xyz?file=1.2.1/setup.exe
  - It will record the download, then redirect the user to github.com/GH_Username/Repo/releases
--- src/worker.js
+++ src/worker.js
@@ -1,26 +1,64 @@
+async function cron(env) {
+  console.log("Cron func is running...");
+  const KV = env.KV;
+  const webhookURL = env.url;
+  var message = ""
+  message += "\n\n";
+  const list = await KV.list();
+  console.log("Key list aquired");
+  for (const { name, expiration } of list.keys) {
+    console.log("Getting value for " + name);
+    const value = await KV.get(name);
+    if (name == "total" | name == undefined | name == "undefined") {
+      console.log("Invalid Repo, skipping");
+    } else {
+    message += (`Repo: ${name}, Count: ${value}\n`);
+    }
+  }
+  console.log("Message generated. Sending to Discord...");
+  console.log(message);
+  
+  await fetch(webhookURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: message,
+    }),
+  })
+    .then((response) => console.log(response))
+    .catch((error) => console.error(error));
+  console.log("Message sent to Discord");
+  return "complete\n";
+}
+
+
+
 export default {
   async fetch(request, env, ctx) {
-    console.log("Worker is running...")
+    console.log("Worker is running...");
     const url = request.url;
+    if (url.includes("testCron")) { console.log("testing cron func"); return new Response(await cron(env));}
     if (url.includes("favicon.ico")) {
       return new Response(null, { status: 404 });
     }
     const KV = env.KV;
     console.log(url);
     const packName = url.split("/reder?url=")[1]; //pack name will include username, e.g niceygylive/example
-    //var packName = "";
-    
+    const fileName = url.split("?file=")[1]; //e.g V1.1.2/download.zip
+
     console.log(packName);
 
     var oldNum = await KV.get(packName);
     // Record the URL if it is not already recorded in the Worker's storage.
     if (oldNum / oldNum != 1) {
-      console.log("Not found in KV")
+      console.log("Not found in KV");
       await KV.put(packName, 1);
       console.log(packName + " added to KV");
     } else {
-      console.log("Found in KV")
-      var newCount = oldNum*1+1;
+      console.log("Found in KV");
+      var newCount = oldNum * 1 + 1;
       //var newCount = oldNum + 1;
       console.log(packName + " has " + newCount + " downloads");
       await KV.put(packName, newCount);
@@ -28,34 +66,20 @@ export default {
     }
 
     let oldTotal = await KV.get("total");
-    await KV.put("total", oldTotal*1+1);
+    await KV.put("total", oldTotal * 1 + 1);
 
     // Redirect the user to the recorded URL.
-    return await Response.redirect("https://github.com/"+packName+"/releases", 301, {
-      headers: {
-        //"Cache-Control": "no-cache",
-        "Cache-Control": "no-store"
+    return await Response.redirect(
+      "https://github.com/" + packName + "/releases/download" + fileName,
+      301,
+      {
+        headers: {
+          "Cache-Control": "no-store",
+        },
       }
-    });
+    );
   },
   async scheduled(event, env, ctx) {
-    async function cron() {
-      const webhookURL = env.url;
-      const message =
-        "Total Downloads for this week: " + (await KV.get("total"));
-      fetch(webhookURL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          content: message,
-        }),
-      })
-        .then((response) => console.log(response))
-        .catch((error) => console.error(error));
-      await KV.put("total", 0);
-    }
-    ctx.waitUntil(cron());
+    ctx.waitUntil(cron(env));
   },
 };
